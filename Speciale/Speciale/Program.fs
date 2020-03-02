05082d38544b560ecbbec99869ba9e40bc84f993
MatiasDaugaard <matiasdaugaard@gmail.com>
2020-03-02 12:58:42 +0100

    Created project

---
+++ Speciale/Speciale/Program.fs
@@ -0,0 +1,7 @@
+﻿
+// Type declarations
+type Train = string
+type Direction = L | R
+type Location = int
+type Rail = Location * Location
+type SplitRail = Location * Location * Location * Direction
