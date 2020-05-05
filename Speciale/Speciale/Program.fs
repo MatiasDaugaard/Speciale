7280c379d4686a72bcc09dbfaa9ca598950e315b
MatiasDaugaard <matiasdaugaard@gmail.com>
2020-05-05 11:33:28 +0200

    Saves solution in file

--- Speciale/Speciale/Program.fs
+++ Speciale/Speciale/Program.fs
@@ -3,6 +3,7 @@ open FSharpx.Collections
 open Railways.Types
 open Railways.BestFirst
 open Railways.LoadFiles
+open System.IO
 
 
 
@@ -13,9 +14,11 @@ let GameOver s =
                        let locs2 = Set.ofSeq locs1
                        Seq.length locs1 <> Set.count locs2
     | _ -> failwith "GAMEOVER"
-    
 
-let rn = LoadRailway "CopenhagenRealistic.txt"
+
+let filename = "CopenhagenRealistic"
+
+let rn = LoadRailway (filename + ".txt")
 
 let stopWatch = System.Diagnostics.Stopwatch.StartNew()
 let result,x = (Solve rn)
@@ -29,6 +32,31 @@ let PrintTrains s =
 
 Console.WriteLine (sprintf "Time spend in total : %A (ms)" (stopWatch.Elapsed.TotalMilliseconds))
 List.iter (fun s -> if (GameOver s) then Console.WriteLine(sprintf "Something went wrong GameOver") else ()) (List.rev result)
+type B = T | F
+
+let solutionSequence (sol:State list) =
+        let template = "<trains>{0}\n<signals>{1}\n<rails>{2}"
+
+
+        let locs m = List.rev (Map.fold (fun s k v -> v::s) [] m)
+        let bLocs m = List.rev (Map.fold (fun s k v -> if v then T::s else F::s) [] m)
+
+        let row (s) =
+            match s with
+            | S(_,sm,tm,rm,_) -> sprintf "<>\n<trains>%A\n<signals>%A\n<rails>%A\n</>"  (locs tm) (bLocs sm) (bLocs rm)
+            | _ -> failwith "F"
+
+        seq {
+            yield "<solution>"
+            for x in sol  do
+                yield row x
+            yield "</solution>"
+        }
+
+
+let path = Path.Combine(__SOURCE_DIRECTORY__,(filename + ".sol"))
+File.WriteAllLines (path, solutionSequence (List.rev result)) |> ignore
+
 Console.WriteLine(sprintf "Length of solution : %A" (List.length result))
 Console.WriteLine(sprintf "Generated states : %A" (x))
 
